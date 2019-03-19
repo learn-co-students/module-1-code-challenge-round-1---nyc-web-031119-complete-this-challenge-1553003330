155fcd871deac2c3048be0276469d41d5571f59b
J-Agens <johntagens@gmail.com>
2019-03-19 10:51:02 -0400

    Finished challenge

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,59 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def self.find_by_name(name)
+    @@all.find do |customer|
+      customer.full_name == name
+    end
+  end
+
+  def self.find_all_by_first_name(name)
+    @@all.select do |customer|
+      customer.first_name == name
+    end
+  end
+
+  def self.all_names
+    @@all.map do |customer|
+      customer.full_name
+    end
+  end
+
+  # Review.new(customer, restaurant, rating, content)
+  def add_review(restaurant, rating, content)
+    Review.new(self, restaurant, rating, content)
+  end
+
+  ## HELPER METHOD
+  def reviews
+    Review.all.select do |review|
+      review.customer == self
+    end
+  end
+
+  def num_reviews
+    self.reviews.length
+  end
+
+  def restaurants
+    reviews.map do |review|
+      review.restaurant
+    end.uniq
   end
 end
