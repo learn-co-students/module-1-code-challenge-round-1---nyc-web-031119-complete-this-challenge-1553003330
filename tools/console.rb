1e367cc4f854bfe837721c082de1b07fca5fbea6
github username <github email address>
2019-03-19 11:02:17 -0400

    Done

--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,13 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+oscar = Customer.new("Oscar", "Rios")
+nani = Customer.new("Nani", "Shoultz")
+mich = Customer.new("Mich", "Smith")
+
+lulu = Restaurant.new("lulu")
+
+find_by_name(oscar)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
