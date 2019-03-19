8eb200ca10334644b0bb377070f318b69b577739
Jack Hilscher <Jack@Jacks-MacBook-Air.local>
2019-03-19 10:50:24 -0400

    Done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,70 @@
 class Customer
-  attr_accessor :first_name, :last_name
+
+  @@all = []
+
+  attr_accessor :first_name, :last_name, :full_name
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @full_name = "#{first_name} #{last_name}"
+    @@all << self
   end
 
-  def full_name
-    "#{first_name} #{last_name}"
+  # Just moved it to the initializer so @@all would include it
+  # def full_name
+  #   @full_name = "#{first_name} #{last_name}"
+  # end
+
+  def self.all
+    @@all
   end
+
+  def self.all_names
+    self.all.map do |customer|
+      customer.full_name
+    end
+  end
+
+  # Old Code, reworked when I made the full_name method create an instant variable
+  # def self.find_by_name(full)
+  #    first = full.split[0]
+  #    last = full.split[1]
+  #   self.all.find do |customer|
+  #     customer.first_name == first && customer.last_name == last
+  #   end
+  # end
+
+  def self.find_by_name(full)
+    self.all.find do |customer|
+      customer.full_name == full
+    end
+  end
+
+  def self.find_all_by_first_name(first)
+    self.all.select do |customer|
+      customer.first_name == first
+    end
+  end
+
+  def add_review(restaurant, rating, content)
+    Review.new(self, restaurant, rating, content)
+  end
+
+  def reviews
+    Review.all.select do |review|
+      review.customer == self
+    end
+  end
+
+  def num_review
+    self.reviews.length
+  end
+
+  def restaurants
+    reviews.map do |review|
+      review.restaurant
+    end.uniq
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,23 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+tommy = Customer.new("Tommy", "Baby")
+acacia = Customer.new("Acacia", "McGill")
+ernie = Customer.new("Ernest", "Clyde")
+roger = Customer.new("Roger", "Bodger")
+thomas = Customer.new("Tommy", "Hresgard")
+
+no_good = Restaurant.new("Actually No Good")
+italian = Restaurant.new("Vinnie Joey Roberto's Fresh Real Original")
+
+tommy.add_review(no_good, 4.0, "better than expected")
+tommy.add_review(no_good, 1.5, "changed my mind")
+roger.add_review(no_good, 0, "utter trash")
+ernie.add_review(no_good, 7, "I'm the chef, it's all tasty!")
+acacia.add_review(italian, 5.0, "It's all one guy, very impressed")
+thomas.add_review(italian, 2.6, "There's no sushi here")
+roger.add_review(italian, 4, "No bugs in my bolognese")
+ernie.add_review(italian,  6, "I don't know how ratings work")
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
