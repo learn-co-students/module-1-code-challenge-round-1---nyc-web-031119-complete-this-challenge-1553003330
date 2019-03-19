0b8b3bc696b30702225577531f37e681902a11b3
github username <github email address>
2019-03-19 10:41:40 -0400

    Done.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,55 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+  @@all = []
+  @@all_names = []
+  @@first_names = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
+    @@all_names << self.full_name
+    @@first_names << self.first_name
   end
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    all_names.find{|full_name| full_name == name}
+  end
+
+  def self.find_by_first_name(name)
+    first_names.select{|first_name| first_name == name}
+  end
+
+  def self.first_names
+    @@first_names
+  end
+
+  def self.all_names
+    @@all_names
+  end
+
+  def add_review(restaurant, content, rating)
+    Review.new(self,restaurant,content,rating)
+  end
+
+  def reviews
+    Review.all.select{|review| review.customer == self}
+  end
+
+  def num_reviews
+    reviews.length
+  end
+
+  def restaurants
+    reviews.map{|review| review.restaurant}.uniq
   end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,48 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+jeff = Customer.new('jeff','bezos')
+gavin = Customer.new('gavin','belson')
+richard = Customer.new('richard','hendricks')
+richard = Customer.new('richard','ross')
+gavin = Customer.new('gavin','belson')
+
+burgerking = Restaurant.new('burger king')
+wendys = Restaurant.new('wendys')
+mcdonalds = Restaurant.new('mcdonalds')
+popeyes = Restaurant.new('popeyes')
+
+#### Customer
+# Customer.all
+# Customer.find_by_name('gavin belson')
+# Customer.find_by_first_name('richard')
+# Customer.all_names
+#
+# jeff.add_review(burgerking,'sucks',2)
+# jeff.add_review(burgerking,'still sucks',1)
+# jeff.add_review(popeyes,'rocks',5)
+# gavin.add_review(burgerking,'awesome',5)
+#
+# jeff.num_reviews
+# jeff.restaurants
+#
+# #### Restaurant
+# Restaurant.all
+# Restaurant.find_by_name('popeyes')
+# burgerking.customers
+# burgerking.reviews
+# burgerking.average_star_rating
+# burgerking.longest_review
+#
+# 
+# #### Review
+# Review.all
+#
+# Review.all.map{|r| r.customer}
+# Review.all.map{|r| r.restaurant}
+# Review.all.map{|r| r.rating}
+# Review.all.map{|r| r.content}
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
