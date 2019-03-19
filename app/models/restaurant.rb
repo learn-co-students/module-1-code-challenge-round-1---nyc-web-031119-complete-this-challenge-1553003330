10590b5ba65d5da1041c833dd7bd6c1f202c37b2
Theo Weller <weller.theodor@gmail.com>
2019-03-19 11:01:57 -0400

    Didn't finish

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,26 @@
 class Restaurant
   attr_accessor :name
 
+	@@all = []
+  
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+	@@all
+  end
+
+def self.find_by_name(name)
+Restaurant.all.find {|restaurant| restaurant.name == name}
+end
+
+def customers
+Review.all.map do |review| 
+	return review.customer.full_name if review.restaurant == self
+end
+end
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,20 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+#Initiate customer
+robeefa = Customer.new("robeefa", "quincy")
+jershumpus = Customer.new("jershumpus", "pebblerot")
+jershumpus_s = Customer.new("jershumpus", "stutterbottom")
+
+#Initiate resturant
+crusty_crab = Restaurant.new("crusty crab")
+rusty_ricks = Restaurant.new("rusty ricks")
+
+#Reviews
+robeefa.add_review(rusty_ricks, 5, "Great service and attention. 10/10 would reccomend!")
+robeefa.add_review(crusty_crab, 1, "Owner was too crabby.")
+robeefa.add_review(crusty_crab, 1, "Cashier was too cynical.")
+jershumpus.add_review(rusty_ricks, 4, "Best ice water soup in town!")
+jershumpus_s.add_review(rusty_ricks, 5, "Cleanest bathrooms!")
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
